6d31b3c5ac1dd02a21ec0f138fadc981beda46d8
cnttkhs <cnt.tkhs@gmail.com>
2017-07-17 03:01:27 +0900

    Add mongoDB

---
+++ chatapp/public/javascripts/client.js
@@ -0,0 +1,57 @@
+$(function() {
+
+  var socket = io('http://localhost:3000');
+
+  var userName = $('#username').text();
+  var $timeline = $('#timeline');
+  var $msgForm = $('#message');
+  var $loginBtn = $('#login');
+  var $flash = $('#flash');
+
+  socket.on('all messages', function(data) {
+    if(!data.length || !$timeline.length) return;
+    data.forEach(function(doc) {
+      addMessage(doc)
+    });
+  });
+
+  socket.on('user joined', function(name) {
+    $flash.text(name + ' joined!');
+    $flash.fadeIn('slow', function() {
+      $(this).delay(3000).fadeOut('slow');
+    });
+  });
+
+  socket.on('new message', function(data) {
+    addMessage(data)
+  });
+
+  $loginBtn.on('click', function() {
+    var name = $('#name').val();
+    socket.emit('login', name);
+    window.location.href = '/chat/' + name;
+  });
+
+  $msgForm.keypress(function(e) {
+    if(e.which !== 13) return;
+    var val = $(this).val();
+    $(this).val('');
+    socket.emit('post message', {
+      name: userName,
+      message: val,
+      time: new Date()
+    });
+  });
+
+  var addMessage = function(data) {
+    $timeline.prepend('<li><span>' + data.name + '</span><span>' + data.message + '</span><span>' + timeFormat(data.time) +'</span></li>')
+  };
+
+  var timeFormat = function(time) {
+    var t = new Date(time)
+    var h = t.getHours();
+    var m = t.getMinutes();
+    var s = t.getSeconds();
+    return h + ':' + m + ':' + s;
+  }
+});
